6de533a2780dd337292a912a23ec578ca349eb80
benignstar <che0009@naver.com>
2017-05-22 20:57:00 +0900

    #3

--- app/src/main/java/benignstar/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mInflater = getMenuInflater();
         mInflater.inflate(R.menu.menu1, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_blue :
+
+                break;
+            case R.id.item_red :
+                break;
+            case R.id.item_yellow :
+                break;
+            case R.id.item_but_rotate :
+                break;
+            case R.id.item_but_size:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
