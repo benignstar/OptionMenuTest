428b55dbe433a7d41a2cac9844a153ecebf8a10e
benignstar <che0009@naver.com>
2017-05-23 19:55:38 +0900

    #4

--- app/src/main/java/benignstar/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,10 +1,15 @@
 package benignstar.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,23 +26,30 @@ public class MainActivity extends AppCompatActivity {
         mInflater.inflate(R.menu.menu1, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        Button but_ok=(Button)findViewById(R.id.but_ok);
+        LinearLayout linear=(LinearLayout)findViewById(R.id.linear);
+
         switch (item.getItemId()){
             case R.id.item_blue :
-
-                break;
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
             case R.id.item_red :
-                break;
+                linear.setBackgroundColor(Color.RED);
+                return true;
             case R.id.item_yellow :
-                break;
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
             case R.id.item_but_rotate :
-                break;
+                but_ok.setRotation(but_ok.getRotation()+15);
+                return true;
             case R.id.item_but_size:
-                break;
-
+                but_ok.setScaleX(but_ok.getScaleX()*1.2f);
+                but_ok.setScaleY(but_ok.getScaleY()*1.2f);
+                return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
